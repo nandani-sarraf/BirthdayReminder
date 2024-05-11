0301919d2e85b6df76dc8868677a1d3b29689ea8
Nandani Kumari <nandanisarraf002@gmail.com>
2024-05-11 09:55:49 +0530

    Update

--- src/demoData.js
+++ src/demoData.js
@@ -2,31 +2,31 @@ const demoData = {
   people: [
     {
       id: 1,
-      name: 'Derek Green',
+      name: 'John',
       birthday: '20.05.1992',
       picture: './assets/man-3.png',
     },
     {
       id: 2,
-      name: 'Lewis Milton',
+      name: 'Bill Gates',
       birthday: '15.11.1980',
       picture: './assets/man-4.png',
     },
     {
       id: 3,
-      name: 'Michael John',
+      name: 'Reddish',
       birthday: '20.10.1985',
       picture: './assets/man-1.png',
     },
     {
       id: 4,
-      name: 'Niho Kerr',
+      name: 'Michal',
       birthday: '06.04.1993',
       picture: './assets/girl-1.png',
     },
     {
       id: 5,
-      name: 'Tom Bloom',
+      name: 'Tommy',
       birthday: '12.02.1990',
       picture: './assets/man-2.png',
     },
